ad94ffbaee8ea3ce095daa0e15a2be37dd1100d8
T-Royale <141929186+T-Royale@users.noreply.github.com>
2025-12-05 11:35:58 +0100

    Day 4: Printing department (Part 2)

--- Day-4: Printing department/day4.h
+++ Day-4: Printing department/day4.h
@@ -3,6 +3,6 @@
 const int MAX_LINE_LEN = 256;
 const int MAX_LINE_COUNT = 256;
 
-#define increment_count_if_valid(y,x) if(grid[y][x] == '@') roll_count++;
+#define increment_count_if_valid(y,x) if(grid[y][x] == '@' || grid[y][x] == 'x') roll_count++;
 
 bool is_valid_paper_roll(char grid[MAX_LINE_COUNT][MAX_LINE_LEN], int pos_x, int pos_y, int max_x, int max_y);
