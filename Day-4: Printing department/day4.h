a50b8ab915be4a409220ae6e5992e6d1eee75518
T-Royale <141929186+T-Royale@users.noreply.github.com>
2025-12-05 11:35:30 +0100

    Day 4: Printing department (Part 1)

---
+++ Day-4: Printing department/day4.h
@@ -0,0 +1,8 @@
+#include "../clibs.h"
+
+const int MAX_LINE_LEN = 256;
+const int MAX_LINE_COUNT = 256;
+
+#define increment_count_if_valid(y,x) if(grid[y][x] == '@') roll_count++;
+
+bool is_valid_paper_roll(char grid[MAX_LINE_COUNT][MAX_LINE_LEN], int pos_x, int pos_y, int max_x, int max_y);
---
+++ Day-4: Printing department/part1.c
@@ -0,0 +1,48 @@
+#include "day4.h"
+
+int main(){
+    int result = 0;
+    FILE* input = fopen("input.txt", "r");
+    char grid[MAX_LINE_COUNT][MAX_LINE_LEN];
+    int row_count = 0;
+    // Fill grid
+    while (row_count < MAX_LINE_COUNT && fgets(grid[row_count], MAX_LINE_LEN, input)) {
+        grid[row_count][strcspn(grid[row_count], "\n")] = '\0';
+        row_count++;
+    }
+    // Get valid paper rolls
+    for(int i = 0; i < row_count; i++){
+        int len = strlen(grid[i]);
+        for(int j = 0; j < len; j++){
+            if(grid[i][j] == '@'){
+                if(is_valid_paper_roll(grid, j, i, len, row_count)){
+                    result++;
+                }
+            }
+        }
+    }
+    printf("The result is: %d\n", result);
+    fclose(input);
+    return 0;
+}
+
+bool is_valid_paper_roll(char grid[MAX_LINE_COUNT][MAX_LINE_LEN], int pos_x, int pos_y, int max_x, int max_y){
+    int roll_count = 0;
+    //Upper-left
+    if(pos_x-1 >= 0 && pos_y-1 >= 0) increment_count_if_valid(pos_y-1, pos_x-1);
+    // Upper
+    if(pos_y-1 >= 0) increment_count_if_valid(pos_y-1, pos_x);
+    // Upper-right
+    if(pos_y-1 >= 0 && pos_x+1 < max_x) increment_count_if_valid(pos_y-1, pos_x+1);
+    // Right
+    if(pos_x+1 < max_x) increment_count_if_valid(pos_y, pos_x+1);
+    // Lower-right
+    if(pos_y+1 < max_y && pos_x+1 <= max_x) increment_count_if_valid(pos_y+1, pos_x+1);
+    // Down
+    if(pos_y+1 < max_y) increment_count_if_valid(pos_y+1, pos_x);
+    // Lower-Left
+    if(pos_y+1 < max_y && pos_x-1 >= 0) increment_count_if_valid(pos_y+1, pos_x-1);
+    // Left
+    if(pos_x-1 >= 0) increment_count_if_valid(pos_y, pos_x-1);
+    return (roll_count < 4);
+}
