47e4a77ce7c958a3459caf8310f4c95c46c336db
T-Royale <141929186+T-Royale@users.noreply.github.com>
2025-12-10 12:31:06 +0100

    Day 6: Trash Compactor (Part 1)

---
+++ Day-6: Trash Compactor/part1.c
@@ -0,0 +1,53 @@
+#include "day6.h"
+
+int main(void) {
+    FILE* input = fopen("input.txt", "r");
+    uint64_t result = 0;
+    char lines[NUMBERS_PER_COL+1][4092];
+    for(int i = 0; i <= NUMBERS_PER_COL; i++) fgets(lines[i], 4092, input);
+    int len = strlen(lines[0]);
+
+    int problem_starts[MAX_PROBLEMS] = {0};
+    int problem_count = 0;
+    for(int i = 0; i < len; i++){
+        if(lines[NUMBERS_PER_COL][i] == '+' || lines[NUMBERS_PER_COL][i] == '*'){
+            problem_starts[problem_count++] = i;
+        }
+    }
+    problem_t problems[MAX_PROBLEMS] = {0};
+    for(int i = 0; i < problem_count; i++){
+        int start = problem_starts[i];
+        int end = (i + 1 < problem_count) ? problem_starts[i + 1] : len;
+
+        int problem_len = end - start;
+        char buff[problem_len+1];
+        for(int j = 0; j < NUMBERS_PER_COL; j++){
+            memcpy(buff, &lines[j][start], problem_len);
+            buff[problem_len] = '\0';
+            problems[i].nums[j] = atoi(buff);
+        }
+        problems[i].op = lines[NUMBERS_PER_COL][problem_starts[i]];
+    }
+
+    for(int i = 0; i < problem_count; i++){
+        uint64_t problem_result = problems[i].nums[0];
+        for(int j = 1; j < NUMBERS_PER_COL; j++){
+            switch(problems[i].op){
+                case '+':
+                    problem_result += problems[i].nums[j];
+                    break;
+                case '*':
+                    problem_result *= problems[i].nums[j];
+                    break;
+                default:
+                    printf("Not a valid operator\n");
+                    break;
+            }
+        }
+        result += (uint64_t)problem_result;
+    }
+
+
+    printf("El resultado final es %" PRIu64 "\n", result);
+    return 0;
+}
